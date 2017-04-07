22d3c203d0cad0d78e399c605cfaf6dc3db45fd4
manish181192 <manishvidyasagar@gmail.com>
2017-04-07 15:27:40 +0530

    Include Placeholders

--- Main.py
+++ Main.py
@@ -1,23 +1,20 @@
 import tensorflow as tf
 from multiGPU_Framework import get_cpu_variable, get_cpu_variable_shape, multiGPU_Framework
 
-def loss_prediction_function(batch):
+def loss_prediction_function():
     '''
-    :param batch: List of Placeholders
+    :param : None
     :return: loss, prediction
     
     Note :
             1 .use get_cpu_variable instead of tf.Variable
-            OR get_cpu_variable_shape to specify shape
-            (In case initializer doesnt accept shape as argument)
+            OR get_cpu_variable_shape to specify shape(In case 
+            initializer doesnt accept shape as argument)
 
             2. Add Loss to collection
             Use tf.add_to_collection('losses', loss)
     '''
-    #sequence same as batches
-    input = batch[0] #Input
-    input_y = batch[1] #Labels
-    dropout_keepprob = tf.constant(0.5)
+    #Placeholder
     '''
       Calculate Prediction and Loss
     '''
